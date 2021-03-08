6c947c76253a92e13e868609ab8052cd9bc99e46
ColeDavis99 <coledavis6499@gmail.com>
2021-03-08 17:07:10 -0600

    On step 11

--- HW4.py
+++ HW4.py
@@ -1,6 +1,9 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import community
 import math
+import matplotlib.cm as cm
+from networkx.algorithms.community import centrality as c
 
 def drawGraph(g, node_colors=["Red"], with_labels=True):
 	pos = nx.spring_layout(g)
@@ -18,7 +21,7 @@ fin=open("GameOfThrones.txt", 'rb')
 G = nx.read_edgelist('GameOfThrones.txt', nodetype=str, delimiter=",", data=(("weight", int),("season", int)))
 fin.close()
 
-drawGraph(G)
+# drawGraph(G)
 
 
 '''
@@ -128,7 +131,118 @@ for node in G.nodes():
 	else:
 		node_colors.append("Blue")
 
-drawGraph(G, node_colors, False)
+# drawGraph(G, node_colors, False)
+
+
+
+'''
+######################################################################################################################################
+Step 8 output: Louvain Method
+a) Output the number of communities, the size of largest community, size of smallest community, and modularity of this partitioning
+######################################################################################################################################'''
+partition = community.best_partition(G)
+modularity = community.modularity(partition, G)
+
+
+numPartitions = 0
+populationList = dict()
+
+#Loop through the Louvain partition
+for node in partition:
+	if(partition[node] > numPartitions):		#Get the number of partitions
+		numPartitions = partition[node]
+
+	if(partition[node] not in populationList):	#Count the number of people in each partition
+		populationList[partition[node]] = 1
+	else:
+		populationList[partition[node]] += 1
+
+
+#Loop through the population list of each community
+maxPop = 0
+minPop = len(list(G.nodes()))+1
+for key in populationList:
+	if(populationList[key] > maxPop):
+		maxPop = populationList[key]
+	if(populationList[key] < minPop):
+		minPop = populationList[key]
+
+
+print("\nThere are " + str(numPartitions) + " communities using the Louvain method.")
+print("The smallest community has " + str(minPop) + " people.")
+print("The largest community has " + str(maxPop) + " people.")
+print("The modularity of the Louvain partitioning is " + str(modularity))
+
+
+'''
+#######################################################################
+Step 9 output:
+a) Display the Louvain graph color coded 
+#######################################################################'''
+color_list = list()
+for node in G.nodes():
+	if(partition[node] == 0):
+		color_list.append("Red")
+	elif(partition[node] == 1):
+		color_list.append("Blue")
+	elif(partition[node] == 2):
+		color_list.append("Orange")
+	elif(partition[node] == 3):
+		color_list.append("Blue")
+	elif(partition[node] == 4):
+		color_list.append("Purple")
+	elif(partition[node] == 5):
+		color_list.append("Green")
+	elif(partition[node] == 6):
+		color_list.append("Brown")
+
+# drawGraph(G, color_list, False)
+
+
+'''
+#######################################################################
+Step 11 output: Girvan-Newman
+a) Output the number of communities, the size of largest community, size of smallest community, and modularity of this partitioning
+#######################################################################'''
+components = c.girvan_newman(G)
+i=0
+for row in components:
+	if(i==0):
+		finalResult = row
+	# print(row)
+	i += 1
+
+minCommunity = len(list(G.nodes))+1
+maxCommunity = 0
+ctr = 0
+partitions = dict()
+L = list(finalResult)
+p=0
+for comp in L:
+	for entry in comp:
+		partitions[entry] = p
+		ctr += 1
+	if(ctr < minCommunity):
+		minCommunity = ctr
+	if(ctr > maxCommunity):
+		maxCommunity = ctr
+	p += 1
+	ctr = 0
+
+print(partitions)
+
+print("\nThere are " + str(p) + " communitites using the Girvan-Newman method.")
+print("The smallest community has " + str(minCommunity) + " people.")
+print("The largset community has " + str(maxCommunity) + " people.")
+print("The modularity of the Girvan-Newman partitioning is " + str(community.modularity(partitions, G)))
+
+
+'''
+#######################################################################
+Step 12 output:
+a) The Girvan-Newman graph color coded
+#######################################################################'''
+
 
 
 '''
